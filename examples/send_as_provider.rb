0766adc6e7584341b88f0f27a5b79d8b068a894d
Jonathan George <jonathan@jdg.net>
2010-05-22 10:46:03 -0500

    Add two additional examples to the Provider API, and fix namespacing.

--- examples/send_as_provider.rb
+++ examples/send_as_provider.rb
@@ -1,16 +1,19 @@
 require 'rubygems'
 require 'boxcar_api'
 
-provider_key = 'xyz'
-provider_secret = 'xyz'
+SETTINGS = {
+  :provider_key => 'xyz',
+  :provider_secret => 'xyz',
+  :email => 'user@example.com'
+}
 
-bp = Provider.new(provider_key, provider_secret)
+bp = BoxcarAPI::Provider.new(SETTINGS[:provider_key], SETTINGS[:provider_key])
 
 #### Send an invitation to a user by their e-mail address, to add your service.
-res = bp.subscribe('user@example.com')
+res = bp.subscribe(SETTINGS[:email])
 
 if res.code == 200
-  puts "Success!  You sent an invitation to user@example.com to add your service."
+  puts "Success!  You sent an invitation to #{SETTINGS[:email]} to add your service."
 else
   puts "Problem!  HTTP status code: #{res.code}.  Check the API docs!"
 end
@@ -25,19 +28,41 @@ else
 end
 
 #### Deliver a personalized notification to a subscriber by email.
-res = bp.notify("user@example.com", "This is an example message.", "from")
+res = bp.notify(SETTINGS[:email], "This is an example message.", "from")
 
 if res.code == 200
-  puts "Success!  You sent a broadcast message to everyone using your service."
+  puts "Success!  You sent a personalized message to #{SETTINGS[:email]}."
+else
+  puts "Problem!  HTTP status code: #{res.code}.  Check the API docs!"
+end
+
+#### Deliver a personalized notification to a subscriber by email, but only deliver it once!
+# This includes the from_remote_service_id for the user, which will ensure it's only delivered one time.
+res = bp.notify(SETTINGS[:email], "This is an example message.", "from", "123")
+
+if res.code == 200
+  puts "Success!  You sent a personalized message to #{SETTINGS[:email]}."
+else
+  puts "Problem!  HTTP status code: #{res.code}.  Check the API docs!"
+end
+
+#### Deliver a personalized notification to a subscriber by email, including a redirect payload
+# Redirect payloads are what replaces "::user::" in your Boxcar redirect URL.
+
+res = bp.notify(SETTINGS[:email], "This is an example message.", "from", nil, "jdg")
+if res.code == 200
+  puts "Success!  You sent a personalized message with a redirect payload to #{SETTINGS[:email]}."
 else
   puts "Problem!  HTTP status code: #{res.code}.  Check the API docs!"
 end
 
 #### Deliver a personalized notification to a subscriber by their service token/secret.
-res = bp.notify_service(token, secret, "This is an example message.", "from")
+#### This works, just commented out because it's much easier to use e-mail addresses.
+#
+#res = bp.notify_service(token, secret, "This is an example message.", "from")
 
-if res.code == 200
-  puts "Success!  You sent a broadcast message to everyone using your service."
-else
-  puts "Problem!  HTTP status code: #{res.code}.  Check the API docs!"
-end
+#if res.code == 200
+#  puts "Success!  You sent a broadcast message to everyone using your service."
+#else
+#  puts "Problem!  HTTP status code: #{res.code}.  Check the API docs!"
+#end
