1eb6efba4020cdd0b92bf0026206410286d02dfc
Jesus Lopes <jlopes@zigotto.com.br>
2010-11-15 18:00:20 -0200

    More tests for User class.

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -7,13 +7,30 @@ describe BoxcarAPI::User do
   end
 
   describe "#notify" do
-    it "should sending the notification" do
-      box = BoxcarAPI::User.new(@config['email'], @config['password'])
-      box.notify("message").code.should == 200
+    before :each do
+      @box = BoxcarAPI::User.new(@config['email'], @config['password'])
     end
     it "should return error when invalid parameters" do
       box = BoxcarAPI::User.new('myuser', 'mypassWord')
       box.notify("other message").code.should == 401
+    end
+    it "should sending the notification" do
+      @box.notify("message").code.should == 200
+    end
+    it "should accept from_screen_name" do
+      @box.notify("my message", "jtadeulopes").code.should == 200
+    end
+    it "should accept from_remote_service_id" do
+      @box.notify("my message with unique", "jtadeulopes", "uniquely_identify").code.should == 200
+    end
+    it "should not be delivered, because you've already gotten it" do
+      @box.notify("message duplicate", "jtadeulopes", "uniquely_identify").code.should == 400
+    end
+    it "should accept source_url" do
+      @box.notify("message with url", "jtadeulopes", nil, "http://facebook.com").code.should == 200
+    end
+    it "should accept icon_url" do
+      @box.notify("message with picture", "jtadeulopes", nil, "http://facebook.com", "http://graph.facebook.com/jtadeulopes/picture").code.should == 200
     end
   end
 
