0c9a1510d2d2d564973a9bd98dbcc10de8785321
Kevin Griffin <xgriffin.kevin@gmail.com>
2011-09-26 16:31:56 -0500

    Make examples more straightforward.

--- examples/send_as_provider.rb
+++ examples/send_as_provider.rb
@@ -1,81 +1,50 @@
 require 'rubygems'
-require 'boxcar_api'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "/lib/boxcar_api"))
 
-SETTINGS = {
-  :provider_key => 'xyz',
-  :provider_secret => 'xyz',
-  :email => 'user@example.com'
-}
+  provider_key    = 'key'
+  provider_secret = 'secret'
+  email           = 'a_users_email'
 
-bp = BoxcarAPI::Provider.new(SETTINGS[:provider_key], SETTINGS[:provider_secret])
+provider = BoxcarAPI::Provider.new(provider_key, provider_secret)
 
-#### Send an invitation to a user by their e-mail address, to add your service.
-res = bp.subscribe(SETTINGS[:email])
-
-if res.code == 200
-  puts "Success!  You sent an invitation to #{SETTINGS[:email]} to add your service."
-else
-  puts "Problem!  HTTP status code: #{res.code}.  Check the API docs!"
-end
-
-#### Deliver a broadcast notification to everyone that has added your service.
-res = bp.broadcast("This is an example message.", "from")
-
-if res.code == 200
-  puts "Success!  You sent a broadcast message to everyone using your service."
-else
-  puts "Problem!  HTTP status code: #{res.code}.  Check the API docs!"
-end
+#### Subscribe a user to your service
+res = provider.subscribe(email)
+puts res.code
 
 #### Deliver a personalized notification to a subscriber by email.
-res = bp.notify(SETTINGS[:email], "This is an example message.", "from")
-
-if res.code == 200
-  puts "Success!  You sent a personalized message to #{SETTINGS[:email]}."
-else
-  puts "Problem!  HTTP status code: #{res.code}.  Check the API docs!"
-end
+res = provider.notify(email, "This is an example message")
+puts res.code
 
 #### Deliver a personalized notification to a subscriber by email, but only deliver it once!
 # This includes the from_remote_service_id for the user, which will ensure it's only delivered one time.
-res = bp.notify(SETTINGS[:email], "This is an example message.", "from", "123")
+res = provider.notify(email, "This is an example message. It should only be delivered once.", :from_remote_service_id => "123")
+puts res.code
 
-if res.code == 200
-  puts "Success!  You sent a personalized message to #{SETTINGS[:email]}."
-else
-  puts "Problem!  HTTP status code: #{res.code}.  Check the API docs!"
-end
+#### Deliver a notification with a custom screen name.
+# This is added to the title as the user or application responsible for the message
+res = provider.notify(email, "This is an example message from Rachel.", :from_screen_name => "Rachel")
+puts res.code
 
-#### Deliver a personalized notification to a subscriber by email, including a redirect payload
-# Redirect payloads are what replaces "::user::" in your Boxcar redirect URL.
+#### You can also set a default screen name for notications when initializing your provider
+another_provider = BoxcarAPI::Provider.new(provider_key, provider_secret, "Exmplr")
+res = another_provider.notify(email, "This is an example message from Exmplr")
+puts res.code
 
-res = bp.notify(SETTINGS[:email], "This is an example message.", "from", nil, "jdg")
-if res.code == 200
-  puts "Success!  You sent a personalized message with a redirect payload to #{SETTINGS[:email]}."
-else
-  puts "Problem!  HTTP status code: #{res.code}.  Check the API docs!"
-end
+#### Deliver a notification with a source URL. 
+# This is the URL the user will be taken to when they open your message.
+res = provider.notify(email, "You have an update.", :source_url => "http://example.com/status/1312430")
+puts res.code
 
-#### Deliver a personalized notification to a subscriber by email, including a redirect payload
-# Redirect payloads are what replaces "::user::" in your Boxcar redirect URL.
-# Also include a source_url and an icon_url
+#### Deliver a notification with an icon. 
+res = provider.notify(email, "You have an update.", :icon_url => "http://example.com/public/icon")
+puts res.code
 
-res = bp.notify(SETTINGS[:email], "This is an example message.", "from", nil, "jdg", "http://google.com", 
-                "http://graph.facebook.com/jonathan.george/picture")
-if res.code == 200
-  puts "Success!  You sent a personalized message with a redirect payload, source_url and icon_url to #{SETTINGS[:email]}."
-else
-  puts "Problem!  HTTP status code: #{res.code}.  Check the API docs!"
-end
+#### Deliver a notification to a subset of your subscribers.
+users = ["user1@example.com", "user2@example.com"]
+res = provider.batch_notify(users, "This is a batch example message.", :from_screen_name => "Support", :from_remote_service_id => "123", :source_url => "http://example.com/status/1312430")
+puts res.code
 
 
-#### Deliver a personalized notification to a subscriber by their service token/secret.
-#### This works, just commented out because it's much easier to use e-mail addresses.
-#
-#res = bp.notify_service(token, secret, "This is an example message.", "from")
-
-#if res.code == 200
-#  puts "Success!  You sent a broadcast message to everyone using your service."
-#else
-#  puts "Problem!  HTTP status code: #{res.code}.  Check the API docs!"
-#end
+#### Deliver a broadcast notification to everyone that has added your service.
+res = provider.broadcast("This is an example message.", :icon_url => "http://example.com/public/icon", :from_remote_service_id => "1232331")
+puts res.code
