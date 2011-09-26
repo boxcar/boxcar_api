30639264a70cbe805f8c8044b28f293e452d933d
Kevin Griffin <xgriffin.kevin@gmail.com>
2011-09-26 16:33:00 -0500

    Update for new API and allow passing in optional parameters as key/value pairs.

--- lib/boxcar_api.rb
+++ lib/boxcar_api.rb
@@ -1,54 +1,66 @@
 require 'rubygems'
 require 'httparty'
-require 'digest/md5'
 
 module BoxcarAPI
 
   # For Providers - http://boxcar.io/help/api/providers
   class Provider
     include HTTParty
-    attr_accessor :provider_key, :provider_secret
+    attr_accessor :provider_key, :provider_secret, :screen_name
 
-    def initialize(provider_key, provider_secret)
-      @provider_key = provider_key
+    def initialize(provider_key, provider_secret, screen_name = nil)
+      @provider_key    = provider_key
       @provider_secret = provider_secret
-
+      @screen_name     = screen_name
       self.class.base_uri "https://boxcar.io/devices/providers/#{@provider_key}/notifications"
     end
 
     def subscribe(email)
-      options = { :body => { :email => hashed_email(email) } }
-      self.class.post("/subscribe", options)
+      params = { :body => { :email => email } }
+      self.class.post("/subscribe", params)
     end
 
-    def broadcast(message, from_screen_name = nil, from_remote_service_id = nil, redirect_payload = nil, source_url = nil, icon_url = nil)
-      options = { :body => { :secret => provider_secret,
+    def broadcast(message, options = {:from_screen_name => screen_name, :from_remote_service_id => nil, :source_url => nil, :icon_url => nil})
+      params = { :body => { :secret => provider_secret,
         :notification => {
         :message => message,
-        :from_screen_name => from_screen_name,
-        :from_remote_service_id => from_remote_service_id,
-        :redirect_payload => redirect_payload,
-        :source_url => source_url,
-        :icon_url => icon_url
+        :from_screen_name => options[:from_screen_name],
+        :from_remote_service_id => options[:from_remote_service_id],
+        :source_url => options[:source_url],
+        :icon_url => options[:icon_url]
       } 
       }}
 
-      self.class.post("/broadcast", options)
+      self.class.post("/broadcast", params)
     end
 
-    def notify(email, message, from_screen_name = nil, from_remote_service_id = nil, redirect_payload = nil, source_url = nil, icon_url = nil)
-      options = { :body => { :email => hashed_email(email),
+    def notify(email, message, options = {:from_screen_name => screen_name, :from_remote_service_id => nil, :source_url => nil, :icon_url => nil})
+      params = { :body => { :email => email,
         :notification => {
         :message => message,
-        :from_screen_name => from_screen_name,
-        :from_remote_service_id => from_remote_service_id,
-        :redirect_payload => redirect_payload,
-        :source_url => source_url,
-        :icon_url => icon_url
+        :from_screen_name => options[:from_screen_name],
+        :from_remote_service_id => options[:from_remote_service_id],
+        :source_url => options[:source_url],
+        :icon_url => options[:icon_url]
       } 
       }}
 
-      self.class.post("/", options)
+      self.class.post("/", params)
     end
+    
+    def batch_notify(emails, message, options = {:from_screen_name => screen_name, :from_remote_service_id => nil, :source_url => nil, :icon_url => nil})
+      params = { :body => { :emails => emails,
+        :notification => {
+        :message => message,
+        :from_screen_name => options[:from_screen_name],
+        :from_remote_service_id => options[:from_remote_service_id],
+        :source_url => options[:source_url],
+        :icon_url => options[:icon_url]
+      } 
+      }}
+
+      self.class.post("/", params)
+    end
+  end
 end
 
